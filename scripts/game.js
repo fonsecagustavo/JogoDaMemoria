5d8c25f2798367430bcdf9bb7839309e97ee0b97
Gustavo Fonseca <44001877+fonsecagustavo@users.noreply.github.com>
2021-05-18 14:21:54 -0300

    Adicionando comentarios

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,15 @@
 let game = {
+    
+    //Se esta sendo verificada as duas cartas
     lockMode: false,
+    
+    //primeira carta 
     firstCard:null,
+    
+    //segunda carta
     secondCard: null,
+    
+    //as tecnologias
     techs: [
         'bootstrap',
         'css',
@@ -14,61 +22,81 @@ let game = {
         'node',
         'react'
     ],
+    
+    //inicializa o cards para ela ser uma variavel global
+    cards: null,
+    
+    //grava a carta clicada 
     setCard: function(id){
        let card = this.cards.filter(card=>card.id===id)[0];
 
+       //verifica se a carta ja foi clicada ou se esta tendo a verificação dos pares
        if(card.flipped  || this.lockMode){
            return false;
        }
 
+       //olha se ja tem a primeira carta, se não e adicionada 
        if(!this.firstCard) {
           this.firstCard = card;
-          this.firstCard.flipped = true;
+          this.firstCard.flipped = true; //isso fala que a carta foi selecionada 
           return true;
-       }else {
+       }else { // se não adiciona na segunda carta 
            this.secondCard = card;
-           this.secondCard.flipped = true;
+           this.secondCard.flipped = true;//isso fala que a carta foi selecionada 
            this.lockMode = true;
            return true;
        }
     },
+    
+    //chaca se as cartas são iguais
     checkMath: function () {
         if(!this.firstCard || !this.secondCard){
             return false
         }
         return this.firstCard.icon === this.secondCard.icon
     },
+    
+    //limpa as duas cartas
     clearCards: function () {
         this.firstCard = null;
         this.secondCard = null;
         this.lockMode = false;
     },
+    
+    //retira o flipped para que possa ser selecionada novamente 
     unflipCards: function() {
         this.firstCard.flipped = false;
         this.secondCard.flipped = false;
         this.clearCards();
     },
+    
+    //checa se todas as cartas ja foram flipped e quando não restar nenhuma e game Over
     checkGameOver: function () {
         
         return this.cards.filter(card=>!card.flipped).length == 0;
     },
-
-
-    cards: null,
+    
+    //Aqui são feitas as chamadas para a criação de cada carta para cada tecnologia crinado o par
     createCardsFromTechs: function() {
 
+        //declara um array vazio, para que possa ser adicionado 
         this.cards = [];
     
         this.techs.forEach((tech)=> {
+            //cria os pares das cartas
             this.cards.push(this.createPairFromTech(tech))
         })
     
+        //esta comando faz com que retorne os 20 cartas e não 10 pares de cartas 
         this.cards = this.cards.flatMap(pair => pair);
+        //embaralha as cartas
         this.shuffleCards()
 
+        //retorna as cartas ja embaralhas 
         return this.cards;
     },
     
+    //criação dos pares de cada tecnologia
     createPairFromTech: function(tech) {
         return [
             {
@@ -85,18 +113,26 @@ let game = {
         ]
     },
     
+    //cria o id para cada carta, os pares não tem id iguais.
     createIdWithTech: function(tech) {
         return tech + parseInt(Math.random() * 1000)
     },
 
+    //aqui são embaralha as cartas
     shuffleCards: function() {
+        //começa no fim do array
         let currentIndex = this.cards.length;
+        //numero gerado aleatorio
         let randomIndex = 0;
     
+        //vai diminuindo ate chegar ao 0 vai embaralhando de tras pra fente
         while(currentIndex !== 0){
+            //gera um numero aleatorio ate o index da ultima carta embaralhada 
             randomIndex = Math.floor(Math.random() * currentIndex);
+            //diminui o valor da ultima carta
             currentIndex--;
     
+            //faz a substituição das cartas
             [this.cards[randomIndex],this.cards[currentIndex]] = [this.cards[currentIndex],this.cards[randomIndex]]
         }
     
--- scripts/script.js
+++ scripts/script.js
@@ -8,36 +8,39 @@ const ICON = "icon";
 
 startGame();
 
+//função para iniciar o jogo
 function startGame() {
-   
    initializeCards(game.createCardsFromTechs());
 }
 
+//função inicializar a criação das div dos cards
 function initializeCards() {
     let gameBoard = document.getElementById("gameBoard");
     gameBoard.innerHTML = '';
-    
+
     game.cards.forEach(card => {
 
-        let cardElement = document.createElement('div');
-        cardElement.id = card.id;
-        cardElement.classList.add(CARD);
-        cardElement.dataset.icon = card.icon;
+        let cardElement = document.createElement('div'); // cria uma div 
+        cardElement.id = card.id; //adiciona um id
+        cardElement.classList.add(CARD); //adiciona a class 'card'
+        cardElement.dataset.icon = card.icon; // adiciona data-icon o icon que seria o nome da tecnologia
 
-        createCardContent(card,cardElement)
+        createCardContent(card,cardElement) //cria as divs de front e back
 
-        cardElement.addEventListener('click', flipCard)
+        cardElement.addEventListener('click', flipCard)//adiciona o evento de click
 
-        gameBoard.appendChild(cardElement);
+        gameBoard.appendChild(cardElement);//adiciona a div card ao gameBorad
     })
 }
 
+//função chama a função para criar o front e back 
 function createCardContent(card, cardElement){
 
     createCardFace(FRONT, card, cardElement);
     createCardFace(BACK, card, cardElement);
 }
 
+//função que cria as divs de front e de back
 function createCardFace(face, card, element) {
     let cardElementFace = document.createElement('div');
     cardElementFace.classList.add(face);
@@ -46,7 +49,7 @@ function createCardFace(face, card, element) {
         let iconElement = document.createElement('img');
         iconElement.classList.add(ICON);
         iconElement.src = "./assets/images/" + card.icon + ".png";
-        cardElementFace.appendChild(iconElement)
+        cardElementFace.appendChild(iconElement);
     }else {
         cardElementFace.innerHTML = '&lt/&gt';
     }
@@ -55,6 +58,7 @@ function createCardFace(face, card, element) {
 
 }
 
+//função de click, ela adiciona e retira a classe flip e verifica 
 function flipCard() {
    if(game.setCard(this.id)) {
     this.classList.add("flip")
@@ -84,6 +88,7 @@ function flipCard() {
    }
 }
 
+//reinicia o jogo 
 function restart() {
     game.clearCards();
     startGame();
